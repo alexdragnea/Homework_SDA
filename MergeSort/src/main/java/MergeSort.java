cb3a3b24e622f8ee4ba773c81a470b40ecd273e6
Dragnea Alexandru Marian <58791006+alexdragnea@users.noreply.github.com>
2022-04-19 14:08:53 +0300

    Added implementation of MergeSort

---
+++ MergeSort/src/main/java/MergeSort.java
@@ -0,0 +1,78 @@
+public class MergeSort {
+
+    // Merge two sub arrays L and M into array
+    void merge(int[] array, int p, int q, int r) {
+
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = array[p + i];
+        for (int j = 0; j < n2; j++)
+            M[j] = array[q + 1 + j];
+
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = M[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Divide the array into two sub arrays, sort them and merge them
+    void mergeSort(int array[], int left, int right) {
+        if (left < right) {
+
+            int mid = (left + right) / 2;
+
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+
+            merge(array, left, mid, right);
+        }
+    }
+
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 1, 4, 5, 0, 10, 20, 7, 6, 3, 25, 21, 23, 17};
+
+        MergeSort mergeSort = new MergeSort();
+
+        System.out.println("Given array is: ");
+        printArray(array);
+
+        mergeSort.mergeSort(array, 0, array.length - 1);
+
+        System.out.println("\nGiven array after sorting is:");
+        printArray(array);
+    }
+}
